e103c76cb7bc7974b030432383cd4840981d9a9f
Guilherme Buchalla Ferreira <guilhermebuchallaferreira@gmail.com>
2023-03-08 22:14:36 -0300

    feat: add hero and portfolio section content and improve responsiveness

--- js/scripts.js
+++ js/scripts.js
@@ -47,3 +47,5 @@ document.getElementById("close-alert-btn").addEventListener("click", () => {
 // Activate Bootstrap tooltips 
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]');
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl));
+
+
